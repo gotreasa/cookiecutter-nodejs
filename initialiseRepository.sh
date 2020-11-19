aa12fba077560a993ae7a05ca29493335b06272d
Gearoid O'Treasaigh <gearoid.otreasaigh@ibm.com>
2020-11-19 21:37:01 +0000

    fix: naming and messaging corrections

--- initialiseRepository.sh
+++ initialiseRepository.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # Check Git is installed
 if ! git --version; then
-    echo "Git needs to be installed"
+    echo "ERROR: Git needs to be installed"
     exit 1
 else
-    echo "All good with Git"
+    echo "INFO: All good with Git"
 fi
 
 # Setup the NVM path
@@ -13,14 +13,14 @@ export NVM_DIR="$HOME/.nvm"
   [ -s "/usr/local/opt/nvm/etc/bash_completion" ] && . "/usr/local/opt/nvm/etc/bash_completion"  # This loads nvm bash_completion
 # Check if NVM is installed
 if ! nvm version; then 
-    echo "NVM needs to be installed"
+    echo "ERROR: NVM needs to be installed"
     exit 1
 else
-    echo "All good with NVM"
+    echo "INFO: All good with NVM"
 fi
 
 while [ -z "$repositoryName" ]; do
-    echo "What is the name of the repository you need?"
+    echo -e "\n\nWhat is the name of the repository you need?"
     read repositoryName
 done
 
@@ -28,18 +28,20 @@ while [ -z "$gitUser" ]; do
     echo "What is the name of your Git Hub ID?"
     read gitUser
     if [[ $(curl -L -s -o /dev/null -w "%{http_code}" http://github.com/$gitUser) != 200 ]]; then
-        echo "That ID was not found at http://github.com/$gitUser"
+        echo "ERROR: hat ID was not found at http://github.com/$gitUser"
         unset gitUser
     else
-        echo "Your ID was found at http://github.com/$gitUser"
+        echo "INFO: Your ID was found at http://github.com/$gitUser"
     fi
 done
 
 # Clone the template repository to a new repository name
+echo "INFO: Creating the repository"
 git clone git@github.com:gotreasa/templateRepository.git $repositoryName
 cd $repositoryName
 
 # Setup NVM and Node version
+echo "INFO: Installing node"
 nvm install --lts
 nodeVersion=`nvm version` 
 echo $nodeVersion > .nvmrc
@@ -47,15 +49,19 @@ sed -i '' 's/"node": ".*"/"node": "'${nodeVersion}'"/g' package.json
 sed -i '' 's/gotreasa/'${gitUser}'/g' package.json
 sed -i '' 's/templateRepository/'${repositoryName}'/g' package.json
 # Install and update NPM packages
+echo "INFO: Setting up the npm packages"
 npm i
 npx npm-check-updates -u
 npm i
+echo "INFO: Initialising the repository"
+npm init
 
 # Setup git
+echo "INFO: Committing the code"
 rm -rf .git
 git init
-git remote add origin https://github.com:$gitUser/${repositoryName}
+git remote add origin https://github.com/$gitUser/${repositoryName}
 git checkout -b main
 git add .
 git commit -m "feat: setup of the repository"
-git push origin main
+echo "INFO: Repository setup for ${repositoryName} is now complete"
