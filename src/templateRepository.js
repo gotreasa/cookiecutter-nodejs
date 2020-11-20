e360bb45af5d883666d6b4c79e56082f39856460
Gearoid O'Treasaigh <gearoid.otreasaigh@ibm.com>
2020-11-20 11:57:48 +0000

    feat: setup of the implementation and test files

---
+++ src/templateRepository.js
@@ -0,0 +1,3 @@
+class TemplateRepository {
+}
+module.exports = TemplateRepository;
---
+++ test/templateRepository.spec.js
@@ -0,0 +1,8 @@
+const TemplateRepository = require('../src/templateRepository.js');
+
+describe('Name of the group', () => {
+  test('should ...', () => {
+    const templateRepository = new TemplateRepository();
+    expect(templateRepository).toBeDefined();
+  });
+});
