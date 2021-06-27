dc15a6b0dcee5097652751730f3efb040b9c3006
Gearoid O'Treasaigh <gearoid.otreasaigh@ibm.com>
2021-06-27 16:44:38 +0100

    refactor: switch from port 8090 to 9080 for the app

--- src/app.js
+++ src/app.js
@@ -6,6 +6,6 @@ app.use(helmet());
 
 app.get('/api/v1/dummy', (_, response) => (response.status(200).json()));
 
-const server = app.listen('8090', () => {
+const server = app.listen('9080', () => {
   console.log(`🚀 Template NodeJS app listening at http://localhost:${server.address().port}`);
 });
