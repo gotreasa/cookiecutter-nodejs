858879fe992c5a5fbddf7ef04d772b970a0a719c
Gearoid O'Treasaigh <gearoid.otreasaigh@ibm.com>
2021-06-27 17:25:50 +0100

    chore: adding cors to enable Swagger

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const helmet = require('helmet');
+const cors = require('cors');
 
 const app = express();
 app.use(helmet());
+app.use(cors());
 
 app.get('/api/v1/dummy', (_, response) => (response.status(200).json()));
 
