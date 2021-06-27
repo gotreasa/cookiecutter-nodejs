3842922767b9e326aa2aa18c1a008d615ef8d988
Gearoid O'Treasaigh <gearoid.otreasaigh@ibm.com>
2021-06-27 16:41:49 +0100

    feat: set up the dummy API

---
+++ src/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const helmet = require('helmet');
+
+const app = express();
+app.use(helmet());
+
+app.get('/api/v1/dummy', (_, response) => (response.status(200).json()));
+
+const server = app.listen('8090', () => {
+  console.log(`🚀 Template NodeJS app listening at http://localhost:${server.address().port}`);
+});
