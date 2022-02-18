c7639f597ee2581714553a174e504e4a7634762f
Gearoid O'Treasaigh <gearoid.otreasaigh@ibm.com>
2022-02-18 16:11:24 +0000

    refactor: simplify the Jest configuration

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+const { defaults } = require('jest-config');
+
+module.exports = {
+  clearMocks: true,
+  collectCoverage: true,
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 0,
+    },
+  },
+  moduleFileExtensions: [...defaults.moduleFileExtensions, 'feature'],
+  resetMocks: true,
+  resetModules: true,
+  testEnvironment: 'node',
+  testMatch: [...defaults.testMatch, '**/*_steps.js'],
+};
