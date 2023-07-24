cb14cff7df08df33ce549060ef1cb2c18126b6c2
Gearoid O'Treasaigh <gotreasa@users.noreply.github.com>
2023-07-24 18:29:37 +0100

    ci: update the prettier configuration with each of the plugins

--- {{cookiecutter.project_slug}}/.prettierrc.js
+++ {{cookiecutter.project_slug}}/.prettierrc.js
@@ -2,4 +2,9 @@ module.exports = {
   printWidth: 80,
   singleQuote: true,
   trailingComma: 'all',
+  plugins: [
+    '@prettier/plugin-ruby',
+    'prettier-plugin-gherkin',
+    'prettier-plugin-sh',
+  ],
 };
